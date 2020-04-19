c116c7d673e5f6b9ecd9d0774238bfd01b2fbfe5
Akash Salian <salian.akash09@gmail.com>
2020-04-19 03:47:49 -0600

    Added Sound button, Fixed Negative Timer. Bugs: Cannot Stop Music with Sound toggle.

--- my-app/src/Sound.js
+++ my-app/src/Sound.js
@@ -1,2 +1,43 @@
 import React, {Component} from 'react';
+import loud from './soundIcon.png';
+import mute from './soundMute.png';
 
+class Sound extends Component {
+
+
+    toggleSound = (e) => {
+        this.props.sound === "on" ?
+        this.changeSoundAttributes("off",
+        process.env.PUBLIC_URL
+        + mute, e ) :
+        this.changeSoundAttributes("on",
+        process.env.PUBLIC_URL
+        + loud, e )
+  
+
+    }
+
+changeSoundAttributes = (toggle, src,e) => {
+    this.props.setSound(toggle)
+    e.target.src = src
+}
+
+
+render() {
+    return(
+        <>
+        <button className="soundButton grow" onClick={(e) => {
+        this.toggleSound(e)}}>
+            <img src={process.env.PUBLIC_URL + loud}/>
+        </button>
+        </>
+    )
+
+}
+
+
+
+
+}
+
+export default Sound;
--- my-app/src/Timer.js
+++ my-app/src/Timer.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import Time from './Time';
 import fiveMusic from './betterdays5min.wav';
 import tenMusic from './betterday10min.mp3';
+import Sound from './Sound';
 import 'tachyons';
 
 class Timer extends Component {
@@ -23,6 +24,7 @@ class Timer extends Component {
     this.handleStop = this.handleStop.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.handleSound = this.handleSound.bind(this);
+    
 }
 
 //Starts the timer
@@ -40,7 +42,7 @@ handleStart() {
 // Display Start on the console to check functionality
 console.log("Start");
 
- // Control Music for Breaks
+ // Control the background Music for Breaks
 
  this.relaxingFive = new Audio(fiveMusic);
  this.relaxingTen = new Audio(tenMusic);
@@ -51,23 +53,31 @@ console.log("Start");
             if (this.state.buttonActive === "shortBreak") {
                     this.relaxingFive.play();
             } 
-            else if (this.state.buttonActive === "longBreak" && this.state.timerRunning == true) {
+            else if(this.state.sound === "off"){
+              this.relaxingFive.pause();
+            }
+                        
+            if (this.state.buttonActive === "longBreak") {
                     this.relaxingTen.play();
             }
-            else {
-          this.relaxingFive.pause();
+            else if(this.state.sound === "off"){
           this.relaxingTen.pause();
             }
         }
     }
     playMusic()
 }
+//Start Ends here
+
 
 //Stops the timer
 handleStop() {
       console.log("Stop");
       this.setState({timerRunning: false})
       clearInterval(this.timer)
+      this.state.buttonActive === "shortBreak" ? 
+                     this.relaxingFive.pause() : 
+                      this.relaxingTen.pause()
 }
 
 // Resets the Timer
@@ -106,18 +116,20 @@ const playSound = () => {
 }
 
 
-
-
-
-
 componentDidUpdate() {
      if (this.state.interval < 1 && this.state.timerRunning == true) {
         this.handleStop()
         this.handleSound()
+        this.handleReset()
     }
 
 }
 
+setSound = (sound) => {
+this.setState({
+      sound : sound
+   })
+}
 
 
 
@@ -126,13 +138,14 @@ render() {
 return <div className="mid-container" id="mid-container">
       <div className="timer-hud">
         <span className="hud-heading grow" id="pomodoro" onClick={() => this.setState({interval: (25 * 60), buttonActive: "pomodoro"})}> Pomodoro</span>
-        <span className="hud-heading grow" id="shortBreak" onClick={() => this.setState({interval: (0.05 * 60), buttonActive: "shortBreak"})}>Short Break</span>
+        <span className="hud-heading grow" id="shortBreak" onClick={() => this.setState({interval: (5 * 60), buttonActive: "shortBreak"})}>Short Break</span>
         <span className="hud-heading grow" id="longBreak" onClick={() => this.setState({interval: (10* 60), buttonActive: "longBreak"})}>Long Break</span>
       </div>
 
       
       
 <Time time={this.state.interval}  />
+<Sound sound={this.state.sound} setSound={this.setSound} /> 
 
       <div className="timer-controls">
         <span className="control-start grow shadow-4" onClick={this.handleStart}>Start</span>
